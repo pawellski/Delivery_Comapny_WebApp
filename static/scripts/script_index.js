efdb182028d92cdc624faf907002a30f13d50ed8
pawel <spawel114@gmail.com>
2020-10-26 23:40:39 +0100

    Changed colors

--- static/scripts/script_index.js
+++ static/scripts/script_index.js
@@ -4,9 +4,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var rows = document.getElementsByClassName("tbody_packages")[0].getElementsByTagName("tr");
         for (var i = 0; i < rows.length; i++){
             if (i%2 == 0){
-                rows[i].style.backgroundColor = "#808080";
+                rows[i].style.backgroundColor = "#F5DEB3";
             } else {
-                rows[i].style.backgroundColor = "#ffd700";
+                rows[i].style.backgroundColor = "#C0C0C0";
             }
         }
     });
@@ -15,9 +15,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var rows = document.getElementsByClassName("tbody_packages")[0].getElementsByTagName("tr");
         for (var i = 0; i < rows.length; i++){
             if (i%2 == 0){
-                rows[i].style.backgroundColor = "#dcdcdc";
+                rows[i].style.backgroundColor = "#DCDCDC";
             } else {
-                rows[i].style.backgroundColor = "#ffefd5";
+                rows[i].style.backgroundColor = "#FFEFD5";
             }
         }
     });
