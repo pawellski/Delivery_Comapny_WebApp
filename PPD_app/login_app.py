d96333cb713a5d03043dc294716809e4cf550372
pawel <spawel114@gmail.com>
2020-11-22 23:53:21 +0100

    Fixed a few files to execute app.

--- PPD_app/login_app.py
+++ PPD_app/login_app.py
@@ -10,5 +10,6 @@ def index():
 def register():
     return render_template("registration.html")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+@app.route("/login", methods=["GET"])
+def login():
+    return render_template("login.html")
