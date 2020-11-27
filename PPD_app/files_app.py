5c105519e17c646fe6a1c71ddcf9dc61fc66cf17
pawel <spawel114@gmail.com>
2020-11-27 19:51:56 +0100

    Added module responsible for sending packages

--- PPD_app/files_app.py
+++ PPD_app/files_app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, send_file, request, jsonify, make_response
-from flask_jwt_extended import JWTManager, jwt_required
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 from flask_cors import CORS
+from datetime import datetime
+import uuid
 import logging
 import redis
 import os
 
 GET = "GET"
 POST = "POST"
+waybills = "waybills"
 SECRET_KEY = "LOGIN_JWT_SECRET"
 TOKEN_EXPIRES_IN_SECONDS = 120
 
@@ -24,3 +27,84 @@ log = app.logger
 @app.route("/", methods=[GET])
 def index():
     return render_template("index-files.html")
+
+
+@app.route("/add_package", methods=[POST])
+@jwt_required
+def add_package():
+    login = get_jwt_identity()
+    waybills_login = "waybills_" + login
+    form = request.form
+    if add_package_validation(form):
+        now = datetime.now()
+        now_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        unique_id = uuid.uuid4().hex.encode("utf-8")
+
+        db.hset(waybills_login.encode("utf-8"), unique_id, now_string.encode("utf-8"))
+
+        db.hset(unique_id, "sender_name", form.get("sender_name").encode("utf-8"))
+        db.hset(unique_id, "sender_surname", form.get("sender_surname").encode("utf-8"))
+        db.hset(unique_id, "sender_street", form.get("sender_street").encode("utf-8"))
+        db.hset(unique_id, "sender_number", form.get("sender_number").encode("utf-8"))
+        db.hset(unique_id, "sender_postal_code", form.get("sender_postal_code").encode("utf-8"))
+        db.hset(unique_id, "sender_city", form.get("sender_city").encode("utf-8"))
+        db.hset(unique_id, "sender_country", form.get("sender_country").encode("utf-8"))
+        db.hset(unique_id, "sender_phone_number", form.get("sender_phone_number").encode("utf-8"))
+
+        db.hset(unique_id, "recipient_name", form.get("recipient_name").encode("utf-8"))
+        db.hset(unique_id, "recipient_surname", form.get("recipient_surname").encode("utf-8"))
+        db.hset(unique_id, "recipient_street", form.get("recipient_street").encode("utf-8"))
+        db.hset(unique_id, "recipient_number", form.get("recipient_number").encode("utf-8"))
+        db.hset(unique_id, "recipient_postal_code", form.get("recipient_postal_code").encode("utf-8"))
+        db.hset(unique_id, "recipient_city", form.get("recipient_city").encode("utf-8"))
+        db.hset(unique_id, "recipient_country", form.get("recipient_country").encode("utf-8"))
+        db.hset(unique_id, "recipient_phone_number", form.get("recipient_phone_number").encode("utf-8"))
+
+        log.debug(db.hgetall(waybills_login))
+        log.debug(db.hgetall(unique_id))
+
+        return jsonify({"add_package": "Correct"}), 200
+                    
+    else:
+        return jsonify({"add_package": "Incorrect"}), 400
+
+
+def add_package_validation(form):
+    errors = 0
+    if form.get("sender_name").isspace():
+        errors = errors + 1
+    if form.get("sender_surname").isspace():
+        errors = errors + 1
+    if form.get("sender_street").isspace():
+        errors = errors + 1
+    if form.get("sender_number").isspace():
+        errors = errors + 1
+    if form.get("sender_postal_code").isspace():
+        errors = errors + 1
+    if form.get("sender_city").isspace():
+        errors = errors + 1
+    if form.get("sender_country").isspace():
+        errors = errors + 1
+    if form.get("sender_phone_number").isspace():
+        errors = errors + 1
+    if form.get("recipient_name").isspace():
+        errors = errors + 1
+    if form.get("recipient_surname").isspace():
+        errors = errors + 1
+    if form.get("recipient_street").isspace():
+        errors = errors + 1
+    if form.get("recipient_number").isspace():
+        errors = errors + 1
+    if form.get("recipient_postal_code").isspace():
+        errors = errors + 1
+    if form.get("recipient_city").isspace():
+        errors = errors + 1
+    if form.get("recipient_country").isspace():
+        errors = errors + 1
+    if form.get("recipient_phone_number").isspace():
+        errors = errors + 1
+    
+    if errors == 0:
+        return True
+    else:
+        return False
--- PPD_app/static/scripts/script_add_package.js
+++ PPD_app/static/scripts/script_add_package.js
@@ -1,19 +1,165 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-    var HTTP_STATUS = {OK: 200};
-    var URL = "https://localhost:8080/get_token";
+    
+    var tokenURL = "https://localhost:8080/get_token";
+    var packageURL = "https://localhost:8081/add_package";
+    
     var GET = "GET";
+    var POST = "POST";
+
+    var HTTP_STATUS = {OK: 200, BAD_REQUEST: 400};
+    let addPackageForm = document.getElementById("add-package-form");
+
+    var SENDER_NAME_ID = "sender_name";
+    var SENDER_SURNAME_ID = "sender_surname";
+    var SENDER_STREET_ID = "sender_street";
+    var SENDER_NUMBER_ID = "sender_number";
+    var SENDER_POSTAL_CODE_ID = "sender_postal_code";
+    var SENDER_CITY_ID = "sender_city";
+    var SENDER_COUNTRY_ID = "sender_country";
+    var SENDER_PHONE_NUMBER_ID = "sender_phone_number";
+    var RECIPIENT_NAME_ID = "recipient_name";
+    var RECIPIENT_SURNAME_ID = "recipient_surname";
+    var RECIPIENT_STREET_ID = "recipient_street";
+    var RECIPIENT_NUMBER_ID = "recipient_number";
+    var RECIPIENT_POSTAL_CODE_ID = "recipient_postal_code";
+    var RECIPIENT_CITY_ID = "recipient_city";
+    var RECIPIENT_COUNTRY_ID = "recipient_country";
+    var RECIPIENT_PHONE_NUMBER_ID = "recipient_phone_number";
+    var BUTTON_ID = "button-add-package-form";
 
     get_token();
 
+    addPackageForm.addEventListener("submit", function(event) {
+        event.preventDefault();
 
+        console.log("Add Package Form submission stopped.");
 
+        var n = event.srcElement.length;
+        for(var i = 0; i < n; i++) {
+            console.log(event.srcElement[i].value);
+        }
+
+        errors = 0;
+        errors = isElementLengthCorrect(SENDER_NAME_ID, errors);
+        errors = isElementLengthCorrect(SENDER_SURNAME_ID, errors);
+        errors = isElementLengthCorrect(SENDER_STREET_ID, errors);
+        errors = isElementLengthCorrect(SENDER_NUMBER_ID,errors);
+        errors = isElementLengthCorrect(SENDER_POSTAL_CODE_ID, errors);
+        errors = isElementLengthCorrect(SENDER_CITY_ID, errors);
+        errors = isElementLengthCorrect(SENDER_COUNTRY_ID, errors);
+        errors = isElementLengthCorrect(SENDER_PHONE_NUMBER_ID, errors);
+        errors = isElementLengthCorrect(RECIPIENT_NAME_ID, errors);
+        errors = isElementLengthCorrect(RECIPIENT_SURNAME_ID, errors);
+        errors = isElementLengthCorrect(RECIPIENT_STREET_ID, errors);
+        errors = isElementLengthCorrect(RECIPIENT_NUMBER_ID,errors);
+        errors = isElementLengthCorrect(RECIPIENT_POSTAL_CODE_ID, errors);
+        errors = isElementLengthCorrect(RECIPIENT_CITY_ID, errors);
+        errors = isElementLengthCorrect(RECIPIENT_COUNTRY_ID, errors);
+        errors = isElementLengthCorrect(RECIPIENT_PHONE_NUMBER_ID, errors);
+
+        if (errors == 0) {
+            submitAddPackage();
+        } else {
+            showWarning();
+        }
+    });
+
+    function submitAddPackage() {
+        token = "Bearer " + window.localStorage.getItem("access_token");
+        let packageParams = {
+            method: POST,
+            headers: {'Authorization': token},
+            body: new FormData(addPackageForm),
+            redirect: "follow"
+        }
+
+        fetch(packageURL,packageParams)
+            .then(response => getAddPackageResponseData(response))
+            .then(response => displayInConsoleInformation(response))
+            .catch(err => {
+                console.log("Caught error: " + err)
+            });
+
+    }
+
+    function getAddPackageResponseData(response) {
+        let status = response.status;
+
+        if(status === HTTP_STATUS.OK || status === HTTP_STATUS.BAD_REQUEST) {
+            return response.json();
+        } else {
+            console.error("Response status code: " + response.status);
+            throw "Unexcepted response status: " + response.status;
+        }
+    }
+
+    function displayInConsoleInformation(correctResponse) {
+        let status = correctResponse.add_package;
+        console.log("Status: " + status);
+
+        if(status == "Correct") {
+            showInformation();            
+        } else {
+            showWarning();
+        }
+    }
+
+    function showInformation() {
+        removeMessages();
+        let informationElement = document.getElementById("informationMessage");
+        let informationMessage = document.createTextNode("Dodano paczkę do systemu!");
+        let currentElem = document.getElementById(BUTTON_ID);
+        informationElement = document.createElement('span');
+        informationElement.setAttribute("id", "informationMessage");
+        informationElement.appendChild(informationMessage);
+        currentElem.insertAdjacentElement('afterend', informationElement);
+    }
+
+    function showWarning () {
+        removeMessages();
+        let warningElement = document.getElementById("warningMessage");
+        let warningMessage = document.createTextNode("Pola nie mogą być puste!");
+        let currentElem = document.getElementById(BUTTON_ID);
+        warningElement = document.createElement('span');
+        warningElement.setAttribute("id", "warningMessage");
+        warningElement.className = "warning-field";
+        warningElement.appendChild(warningMessage);
+        currentElem.insertAdjacentElement('afterend', warningElement);        
+    }
+
+    function removeMessages() {
+        let informationMessage = document.getElementById("informationMessage");
+        let warningMessage = document.getElementById("warningMessage");
+
+        if (informationMessage !== null) {
+            informationMessage.remove();
+        }
+
+        if (warningMessage !== null) {
+            warningMessage.remove();
+        }
+    }
+
+    function isElementLengthCorrect(element, errors) {
+        let field = document.getElementById(element);
+
+        if (field.value.replace(/\s/g, '').length < 1) {
+            errors = errors + 1;
+            return errors;
+        } else {
+            return errors;
+        }
+    }
+    
+    
+    
     function get_token() {
         let params = {
             method: GET,
             redirect: "follow"
         }
         
-        fetch(URL, params)
+        fetch(tokenURL, params)
             .then(response => getResponseData(response))
             .then(response => setToken(response))
             .catch(err => {
