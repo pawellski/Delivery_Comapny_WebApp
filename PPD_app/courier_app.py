c500861c64e742b93adaf1ab1fbce1a13d2d8292
pawel <spawel114@gmail.com>
2020-12-20 18:50:42 +0100

    Added courier_pickup_package page.

--- PPD_app/courier_app.py
+++ PPD_app/courier_app.py
@@ -11,6 +11,8 @@ db = redis.Redis(host="redis-db", port=6379, decode_responses=True)
 api_app = Api(app = app, version = "0.1", title = "Courier App API", description = "REST-full API for Courier")
 
 login_page_namespace = api_app.namespace("login_page", description = "Login Page API")
+packages_namespace = api_app.namespace("packages", description = "Packages API")
+pickup_package_namespace = api_app.namespace("pickup_package", description = "Pick Up Package API")
 
 log = app.logger
 COURIERS = "couriers"
@@ -18,7 +20,7 @@ COURIERS = "couriers"
 @login_page_namespace.route("/")
 class LoginPage(Resource):
 
-    @api_app.response(200, "parcel_locker_user.html")
+    @api_app.response(200, "courier_login.html")
     @api_app.produces(["text/html"])
     def get(self):
         self.add_couriers()
@@ -38,3 +40,20 @@ class LoginPage(Resource):
             db.sadd(COURIERS, "KR023")
             db.sadd(COURIERS, "KR054")
             db.sadd(COURIERS, "KR091")  
+
+
+@packages_namespace.route("/")
+class Packages(Resource):
+    @api_app.response(200, "courier_packages.html")
+    @api_app.produces(["text/html"])
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template("courier_packages.html"), 200, headers)
+
+@pickup_package_namespace.route("/")
+class PickupPackage(Resource):
+    @api_app.response(200, "courier_pickup_package.html")
+    @api_app.produces(["text/html"])
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template("courier_pickup_package.html"), 200, headers)
