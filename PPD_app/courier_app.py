52c40f6ed23e0373c15906a321e5492215674b48
pawel <spawel114@gmail.com>
2020-12-21 00:26:53 +0100

    Added service for cookies in courier_app.

--- PPD_app/courier_app.py
+++ PPD_app/courier_app.py
@@ -10,15 +10,24 @@ app = Flask(__name__)
 db = redis.Redis(host="redis-db", port=6379, decode_responses=True)
 api_app = Api(app = app, version = "0.1", title = "Courier App API", description = "REST-full API for Courier")
 
-login_page_namespace = api_app.namespace("login_page", description = "Login Page API")
+login_namespace = api_app.namespace("login", description = "Login Page API")
+logout_namespace = api_app.namespace("logout", description = "Logout Page API")
 packages_namespace = api_app.namespace("packages", description = "Packages API")
 pickup_package_namespace = api_app.namespace("pickup_package", description = "Pick Up Package API")
 
 log = app.logger
 COURIERS = "couriers"
+SESSION_ID = "session-id"
+COURIER_SESSIONS = "courier-sessions"
 
-@login_page_namespace.route("/")
-class LoginPage(Resource):
+log = app.logger
+
+@login_namespace.route("/")
+class Login(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument("login", required = True, type=str, help = "Set user login", location="form")
+    parser.add_argument("password", required = True, type=str, help = "Set user password", location="form")
 
     @api_app.response(200, "courier_login.html")
     @api_app.produces(["text/html"])
@@ -27,33 +36,86 @@ class LoginPage(Resource):
         headers = {'Content-Type': 'text/html'}
         return make_response(render_template("courier_login.html"), 200, headers)
 
+    @api_app.doc(responses = {200: "OK"})
+    def post(self):
+        form = request.form
+        login = form.get("login").encode("utf-8")
+        password = form.get("password")
 
+        if db.exists(COURIERS):
+            if db.hexists(COURIERS, login):
+                if db.hget(COURIERS, login) == password:
+                    session_uuid = str(uuid.uuid4())
+                    db.hset(COURIER_SESSIONS, session_uuid, login)
+                    response = make_response({"login": "Ok"}, 200)
+                    response.set_cookie(SESSION_ID, session_uuid, max_age=120, secure=True, httponly=True)
+                    return response
+
+        return {"login": "Reject"}, 400
+                    
     def add_couriers(self):
         if db.exists(COURIERS):
-            if db.exists("KR023") == 0:
-                db.set("KR023", "Pa$$worD1")
-            if db.exists("KR054") == 0:
-                db.sadd("KR054", "Pa$$worD2")
-            if db.exists("KR091") == 0:
-                db.sadd("KR091", "Pa$$worD3")        
+            if db.hexists(COURIERS, "KR023") == 0:
+                db.hset(COURIERS, "KR023", "Pa$$worD1")
+            if db.hexists(COURIERS, "KR054") == 0:
+                db.hset(COURIERS, "KR054", "Pa$$worD2")
+            if db.hexists(COURIERS, "KR091") == 0:
+                db.hset(COURIERS, "KR0091", "Pa$$worD3")
         else:
-            db.sadd(COURIERS, "KR023")
-            db.sadd(COURIERS, "KR054")
-            db.sadd(COURIERS, "KR091")  
+            db.hset(COURIERS, "KR023", "Pa$$worD1")
+            db.hset(COURIERS, "KR054", "Pa$$worD2")
+            db.hset(COURIERS, "KR0091", "Pa$$worD3")
 
+@logout_namespace.route("/")
+class Logout(Resource):
+
+    @api_app.response(200, "courier _login.html")
+    @api_app.produces(["text/html"])
+    def get(self):
+        cookie = request.cookies.get(SESSION_ID)
+        if cookie is not None:
+            db.hdel(COURIER_SESSIONS, cookie)
+        headers = {'Content-Type': 'text-html'}
+        return make_response(render_template("courier_login.html"), 200, headers)
 
 @packages_namespace.route("/")
 class Packages(Resource):
     @api_app.response(200, "courier_packages.html")
     @api_app.produces(["text/html"])
     def get(self):
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template("courier_packages.html"), 200, headers)
+        cookie = request.cookies.get(SESSION_ID)
+        if cookie is not None:
+            if db.hexists(COURIER_SESSIONS, cookie):
+                login = db.hget(COURIER_SESSIONS, cookie)
+                db.hdel(COURIER_SESSIONS, cookie)
+                session_uuid = str(uuid.uuid4())
+                db.hset(COURIER_SESSIONS, session_uuid.encode("utf-8"), login)
+                headers = {'Content-Type': 'text/html'}
+                response = make_response(render_template("courier_packages.html"), 200, headers)
+                response.set_cookie(SESSION_ID, session_uuid, max_age=120, secure=True, httponly=True)
+                return response
+            else:
+                return make_response("Unauthorized", 401)
+        else:
+            return make_response("Unauthorized", 401)
 
 @pickup_package_namespace.route("/")
 class PickupPackage(Resource):
     @api_app.response(200, "courier_pickup_package.html")
     @api_app.produces(["text/html"])
     def get(self):
-        headers = {'Content-Type': 'text/html'}
-        return make_response(render_template("courier_pickup_package.html"), 200, headers)
+        cookie = request.cookies.get(SESSION_ID)
+        if cookie is not None:
+            if db.hexists(COURIER_SESSIONS, cookie):
+                login = db.hget(COURIER_SESSIONS, cookie)
+                db.hdel(COURIER_SESSIONS, cookie)
+                session_uuid = str(uuid.uuid4())
+                db.hset(COURIER_SESSIONS, session_uuid.encode("utf-8"), login)
+                headers = {'Content-Type': 'text/html'}
+                response = make_response(render_template("courier_pickup_package.html"), 200, headers)
+                response.set_cookie(SESSION_ID, session_uuid, max_age=120, secure=True, httponly=True)
+                return response
+            else:
+                return make_response("Unauthorized", 401)
+        else:
+            return make_response("Unauthorized", 401)
